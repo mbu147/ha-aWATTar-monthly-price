cc9f8584c3c78e37c8388b09cc8488e236139a31
Baumert Martin <bmai@tgw-group.com>
2024-11-18 16:57:44 +0100

    fix config_flow connection class

--- custom_components/awattar_monthly_price/config_flow.py
+++ custom_components/awattar_monthly_price/config_flow.py
@@ -13,7 +13,7 @@ class AwattarMonthlyPriceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for aWATTar Monthly Price."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLLING
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
