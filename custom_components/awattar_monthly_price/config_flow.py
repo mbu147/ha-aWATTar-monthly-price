190e237853b4b35d410596eb6f7abf40c1a1238f
Baumert Martin <bmai@tgw-group.com>
2024-11-18 16:54:36 +0100

    update config flow

--- custom_components/awattar_monthly_price/config_flow.py
+++ custom_components/awattar_monthly_price/config_flow.py
@@ -1,7 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import selector
 
 from .const import DOMAIN
 
