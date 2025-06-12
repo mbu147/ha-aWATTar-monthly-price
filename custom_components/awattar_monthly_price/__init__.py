29e10115ae6fe603bd721ebb11be7385ea593da6
Martin Baumert <36038279+mbu147@users.noreply.github.com>
2025-06-12 14:53:27 +0200

    Fix: Replace deprecated async_forward_entry_setup with async_forward_entry_setups to support multiple platforms

--- custom_components/awattar_monthly_price/__init__.py
+++ custom_components/awattar_monthly_price/__init__.py
@@ -15,12 +15,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up aWATTar Monthly Price from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload aWATTar Monthly Price config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
